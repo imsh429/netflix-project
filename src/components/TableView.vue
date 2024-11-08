e3263313d38eaa82aed34892d201dba50ba4e984
imsh429 <sh99429@naver.com>
2024-11-08 11:32:30 +0900

    edit tableview

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -8,8 +8,18 @@
       </tr>
       </thead>
       <tbody>
-      <tr v-for="movie in movies" :key="movie.id">
-        <td><img :src="`https://image.tmdb.org/t/p/w200${movie.poster_path}`" :alt="movie.title" /></td>
+      <tr
+          v-for="movie in movies"
+          :key="movie.id"
+          @click="toggleWishlist(movie)"
+          :class="{ 'wishlist-added': isMovieInWishlist(movie.id) }"
+      >
+        <td>
+          <img
+              :src="`https://image.tmdb.org/t/p/w200${movie.poster_path}`"
+              :alt="movie.title"
+          />
+        </td>
         <td>{{ movie.title }}</td>
       </tr>
       </tbody>
@@ -30,7 +40,13 @@ export default {
   props: {
     movies: Array,
     currentPage: Number,
-    hasMorePages: Boolean
+    hasMorePages: Boolean,
+    isMovieInWishlist: Function // 부모 컴포넌트에서 전달된 함수
+  },
+  methods: {
+    toggleWishlist(movie) {
+      this.$emit("toggleWishlist", movie); // 부모 컴포넌트로 영화 데이터를 전달하여 추천 영화 목록 업데이트
+    }
   }
 };
 </script>
@@ -44,6 +60,17 @@ export default {
   padding: 0.5rem;
   border: 1px solid #ddd;
   text-align: center;
+  cursor: pointer;
+}
+.table-view img {
+  width: 100px;
+  transition: transform 0.3s ease;
+}
+.table-view img:hover {
+  transform: scale(1.05);
+}
+.wishlist-added {
+  border: 2px solid #ff0000; /* 추천 영화 스타일 차별화 */
 }
 .pagination {
   display: flex;
