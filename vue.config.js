b6e1d31c377aba8dfd1fd57d2f119877191c3073
imsh429 <sh99429@naver.com>
2024-12-26 17:49:37 +0900

    add kakao login

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/netflix-project/',
-  devServer: {
-    host: process.env.IP_ADDRESS || 'localhost', // .env 파일의 IP_ADDRESS 사용
-    port: process.env.PORT || 8080, // .env 파일의 PORT 사용
-  },
+  
 })
